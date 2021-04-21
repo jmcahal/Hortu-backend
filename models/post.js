3c973316c5f5d4064be245cd60dcb571c87779b9
James Michael Cahal <michael.cahal@gmail.com>
2021-04-21 17:10:36 -0400

    basic routing complete for post

--- models/post.js
+++ models/post.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const db = require('../db');
+const { sqlForPartialUpdate } = require("../helpers/sql");
 const { BadRequestError, NotFoundError } = require('../expressError');
 
 //  functions for posts
@@ -13,12 +14,102 @@ class Post {
         let query = `SELECT title,
                             post_body AS "postBody",
                             username,
-                            plant_id
+                            plant_id AS "plantId"
                     FROM posts
                     ORDER BY username`;
         const postsRes = await db.query(query);
         return postsRes.rows;
     }
+
+    // Find a post given its id.
+
+    static async get(id) {
+        const postRes = await db.query (`SELECT id,
+                        username,
+                        plant_id AS "plantId",
+                        title,
+                        post_body AS "postBody"
+                    FROM posts
+                    WHERE id = $1`,
+                    [id]);
+        const post = postRes.rows[0];
+
+        if (!post) throw new NotFoundError(`No post with the id: ${id}`)
+
+        return post;
+    };
+
+    // Update a post given its id.
+
+    static async update(id,data) {
+
+        const { setCols, values } = sqlForPartialUpdate(
+            data,
+            {
+                tite: "title",
+                postBody: "post_body"
+            }
+        );
+        const idVarIdx = "$" + (values.length + 1);
+
+        const querySql = `UPDATE posts
+                            SET ${setCols}
+                            WHERE id = ${idVarIdx}
+                            RETURNING username,
+                                plant_id AS "plantId",
+                                title,
+                                post_body AS "postBody"`;
+        const result = await db.query(querySql, [...values, id]);
+        console.log(result);
+        const post = result.rows[0];
+
+        return post;
+
+    };
+
+    // Create a post.
+
+    static async create({
+        username,
+        plantId,
+        title,
+        postBody}) {
+
+        const result = await db.query(
+        `INSERT INTO posts
+        (username, 
+            plant_id, 
+            title, 
+            post_body)
+        VALUES ($1,$2,$3,$4)
+        RETURNING username, 
+            plant_id AS "plantId", 
+            title, 
+            post_body AS "postBody"`,
+        [
+            username,
+            plantId,
+            title,
+            postBody
+        ]
+        );
+        const post = result.rows[0];
+
+        return post;    
+        }
+
+        static async delete(id) {
+        const result = await db.query(
+        `DELETE FROM posts
+        WHERE id = $1
+        RETURNING id`,
+        [id]
+        );
+        const post = result.rows[0];
+
+        if (!post) throw new NotFoundError (`No post with id: ${id}`);
+        };
+
 }
 
-module.exports = Post;
+module.exports = Post; 
--- routes/posts.js
+++ routes/posts.js
@@ -21,5 +21,51 @@ router.get("/", async function (req, res, next) {
         return next(err);
     }
 });
+// GET /[id] => {common_name, sci_name, etc...}
+
+router.get("/:id", async function (req, res, next) {
+    try {
+        const post = await Post.get(req.params.id);
+        return res.json({ post });
+
+    }catch (err) {
+        return next(err);
+    }
+});
+
+// PATCH /[id] => {data} => Return {common_name, sci_name, etc...}
+
+router.patch("/:id", async function (req, res, next) {
+    try {
+        const post = await Post.update(req.params.id, req.body);
+        return res.json({ post });
+
+    }catch (err) {
+        return next(err);
+    }
+});
+
+// POST/ => {data} => Return {common_name, sci_name, etc...}
+
+router.post("/", async function (req, res, next) {
+    try {
+        const post = await Post.create(req.body);
+        return res.status(201).json({ post });
+
+    }catch (err) {
+        return next(err);
+    }
+});
+
+// DELETE/[id]=>{common_name}
+
+router.delete("/:id", async function (req, res, next) {
+    try {
+        await Post.delete(req.params.id);
+        return res.json({deleted: req.params.id });
+    } catch (err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
