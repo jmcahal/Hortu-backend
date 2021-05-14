fbc044b61eece347a294af9df4018e97b16fbe99
J. Michael Cahal <michael.cahal@gmail.com>
2021-05-14 13:25:40 -0400

    slight update to seed file

--- hortu-seed.sql
+++ hortu-seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (id,username, password, first_name, last_name, email, is_admin)
+INSERT INTO users (id, username, password, first_name, last_name, email, is_admin)
 VALUES ( 1,
             'testuser',
             'password',
